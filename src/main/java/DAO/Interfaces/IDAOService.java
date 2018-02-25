63eb38c0915f07a27db6966edac55bc9e4356d02
Unknown <olmedvedskaya@gmail.com>
2018-02-25 21:03:56 +0300

    DAOService

---
+++ src/main/java/DAO/Interfaces/IDAOService.java
@@ -0,0 +1,13 @@
+package DAO.Interfaces;
+
+import Model.Service;
+
+import java.util.List;
+
+public interface IDAOService {
+    Service getServiceById(int id);
+    boolean addService(Service service);
+    boolean delService(Service service);
+    boolean editService(Service service);
+    List<Service> getAllServices();
+}
