c7cc80c7d1815d5f14cf665e972853a8da2cac54
Unknown <olmedvedskaya@gmail.com>
2018-03-04 00:08:46 +0300

    Services Servlet

--- src/main/java/Servlets/ServiceServlet.java
+++ src/main/java/Servlets/ServiceServlet.java
@@ -1,4 +1,27 @@
 package Servlets;
 
-public class ServiceServlet {
+import DAO.DAOAgreements;
+import DAO.DAOService;
+import Model.Service;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet("/services")
+public class ServiceServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
+        DAOService daoService = new DAOService();
+        List<Service> services = daoService.getAllServices();
+
+        req.getRequestDispatcher("services.jsp").forward(req, resp);
+    }
 }
---
+++ web/certificate.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: medve
+  Date: 03.03.2018
+  Time: 20:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Certicates</title>
+</head>
+<body>
+<jsp:include page="navigation.jsp"/>
+
+</body>
+</html>
