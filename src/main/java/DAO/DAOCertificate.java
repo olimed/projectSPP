140158f65230c048f1e7a4e2beae5438c0cf3456
Виола <violetta.shavlis@mail.ru>
2018-04-09 15:11:28 +0300

    DAOCertificate tests completed

--- src/main/java/DAO/DAOCertificate.java
+++ src/main/java/DAO/DAOCertificate.java
@@ -14,7 +14,7 @@ public class DAOCertificate extends DAO implements IDAOCertificate{
     public Certificate getCertificateById(int id){
         Certificate certificate = null;
         try {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `certificates` WHERE certificate_id = ?");
+            PreparedStatement statement = connection.prepareStatement("SELECT certificate_id, certificate_doc, certificate_name, certificate_date, certificate_validity FROM `certificates` WHERE certificate_id = ?");
             statement.setInt(1, id);
 
             ResultSet result = statement.executeQuery();
@@ -38,9 +38,9 @@ public class DAOCertificate extends DAO implements IDAOCertificate{
             PreparedStatement statement = connection.prepareStatement("INSERT INTO `certificates` (certificate_doc, certificate_name, certificate_date, certificate_validity) VALUES(?, ?, ?, ?)");
 
             statement.setString(1, certificate.getCertificate_doc());
-            statement.setString(1, certificate.getCertificate_name());
-            statement.setDate(1, certificate.getCertificate_date());
-            statement.setDate(1, certificate.getCertificate_validity());
+            statement.setString(2, certificate.getCertificate_name());
+            statement.setDate(3, certificate.getCertificate_date());
+            statement.setDate(4, certificate.getCertificate_validity());
             result = statement.executeUpdate() != 0;
             certificate.setCertificate_id(getLastAddedId(statement));
         } catch (SQLException e) {
@@ -52,7 +52,7 @@ public class DAOCertificate extends DAO implements IDAOCertificate{
     public boolean delCertificate(Certificate certificate) {
         boolean result = false;
         try {
-            PreparedStatement statement = connection.prepareStatement("DELETE * FROM `certificates` WHERE certificate_id = ?");
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM `certificates` WHERE certificate_id = ?");
             statement.setInt(1, certificate.getCertificate_id());
             result = statement.executeUpdate() != 0;
         } catch (SQLException e) {
@@ -64,7 +64,7 @@ public class DAOCertificate extends DAO implements IDAOCertificate{
     public boolean editCertificate(Certificate certificate) {
         boolean result = false;
         try{
-            PreparedStatement statement = connection.prepareStatement("UPDATE `certificates` SET certificate_doc = ?, certificate_name = ?, certificate_date = ?, certificate_validity = ? WHERE certificate_id = ?)");
+            PreparedStatement statement = connection.prepareStatement("UPDATE `certificates` SET certificate_doc = ?, certificate_name = ?, certificate_date = ?, certificate_validity = ? WHERE certificate_id = ?");
             statement.setString(1, certificate.getCertificate_doc());
             statement.setString(2, certificate.getCertificate_name());
             statement.setDate(3, certificate.getCertificate_date());
@@ -83,10 +83,11 @@ public class DAOCertificate extends DAO implements IDAOCertificate{
         List<Certificate> certificatesList = new ArrayList<Certificate>();
 
         try{
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `certificates`");
+            PreparedStatement statement = connection.prepareStatement("SELECT certificate_id, certificate_doc, certificate_name, certificate_date, certificate_validity FROM `certificates`");
             ResultSet result = statement.executeQuery();
             while (result.next()){
                 Certificate certificate = new Certificate();
+                certificate.setCertificate_id(result.getInt("certificate_id"));
                 certificate.setCertificate_doc(result.getString("certificate_doc"));
                 certificate.setCertificate_name(result.getString("certificate_name"));
                 certificate.setCertificate_date(result.getDate("certificate_date"));
--- src/main/java/testMainClass.java
+++ src/main/java/testMainClass.java
@@ -1,48 +1,48 @@
 import DAO.DAOAgreements;
+import DAO.DAOCertificate;
 import DAO.DAOService;
 import Model.Agreements;
+import Model.Certificate;
 import Model.Service;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public class testMainClass {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        /*
+        DAOService daoService = new DAOService();
+        Service service = new Service();
+        service.setServ_count_people(1);
+        service.setServ_time(13);
+        service.setServ_description("bla-bla-bla");
+        service.setServ_name("Another one service");
+        service.setServ_price(1500);
+        boolean res= daoService.addService(service);
+        service = daoService.getServiceById(2);
+        service.setServ_price(15555);
+        service.setServ_description("pum-pum");
+        res = daoService.editService(service);
+        service = daoService.getServiceById(100);
 
-        //DAOService daoService = new DAOService();
-        //Service service = new Service();
-        //service.setServ_count_people(1);
-        //service.setServ_time(13);
-        //service.setServ_description("bla-bla-bla");
-        //service.setServ_name("Another one service");
-        //service.setServ_price(1500);
-        boolean res;
-        //res= daoService.addService(service);
-        //////////////////////////////////////////////////////////////////////////////////////////////
-        //service = daoService.getServiceById(2);
-        //////////////////////////////////////////////////////////////////////////////////////////////
-        //service.setServ_price(15555);
-        //service.setServ_description("pum-pum");
-        //////////////////////////////////////////////////////////////////////////////////////////////
-        //res = daoService.editService(service);
-//////////////////////////////////////////////////////////////////////////////////////////////
-        //service = daoService.getServiceById(100);
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-        //List<Service> allServices = daoService.getAllServices();
-        //List<Integer> allTools = daoService.getToolsById(1);
-        //List<Integer> allMaterials = daoService.getMaterialssById(1);
-        //List<Integer> allEquipments = daoService.getEquipmentsById(1);
+        List<Service> allServices = daoService.getAllServices();
+        List<Integer> allTools = daoService.getToolsById(1);
+        List<Integer> allMaterials = daoService.getMaterialssById(1);
+        List<Integer> allEquipments = daoService.getEquipmentsById(1);
+        */
         //TODO Lists of tools/materials/equipments + time_to_use?
-
+//////////////////////////////////////////////////////////////////////////////////////////////
+        /*
         DAOAgreements daoagreement = new DAOAgreements();
         Agreements agreement = new Agreements();
         agreement.setParentServiceId(4);
         agreement.setAgr_templete("pum-pu-rum");
         //??public void setAgr_id(int agr_id)
 
-        res = daoagreement.addAgreements(agreement);
+        boolean res = daoagreement.addAgreements(agreement);
 
         agreement = daoagreement.getAgreementsByID(1);
         agreement.setAgr_templete("pam-pam-pam");
@@ -58,5 +58,34 @@ public class testMainClass {
         res = daoagreement.delAgreements(agreement);
 
         List<Agreements> allAgreements = daoagreement.getAllAgreements();
-    }
+        */
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //??public void setCertificate_id(int certificate_id)
+        DAOCertificate daoCertificate = new DAOCertificate();
+        Certificate certificate = new Certificate();
+
+        certificate.setCertificate_doc("certificate_doc");
+        certificate.setCertificate_name("certificate_name");
+        certificate.setCertificate_date(Date.valueOf(LocalDate.of(2018,4,9)));
+        certificate.setCertificate_validity(Date.valueOf(LocalDate.of(2020,4,9)));
+
+        boolean res = daoCertificate.addCertificate(certificate);
+
+        certificate = daoCertificate.getCertificateById(2);
+        int certificateID = certificate.getCertificate_id();
+        String certificateDoc = certificate.getCertificate_doc();
+        String certificateName = certificate.getCertificate_name();
+        Date certificateDate = certificate.getCertificate_date();
+        Date certificateValidity = certificate.getCertificate_validity();
+
+        certificate.setCertificate_name("Another one name");
+
+        res = daoCertificate.editCertificate(certificate);
+
+        certificate = daoCertificate.getCertificateById(3);
+        daoCertificate.delCertificate(certificate);
+
+        List<Certificate> allCertificates = daoCertificate.getAllCertificates();
+        }
 }
